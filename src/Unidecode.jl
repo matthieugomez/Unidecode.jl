8b4bdaf2ba5ed5835af722f060f4084bb5c0be01
matthieugomez <gomez.matthieu@gmail.com>
2019-01-16 15:46:52 -0500

    Update Unidecode.jl

--- src/Unidecode.jl
+++ src/Unidecode.jl
@@ -2,19 +2,11 @@ module Unidecode
     using REPL, Unicode
     const emoji_dict = Dict((v => k) for (k, v) in REPL.REPLCompletions.emoji_symbols)
     function decode_emoji(x)
-        out = ""
-        for char in graphemes(x)
-            out = out * (char ∈ keys(emoji_dict) ? emoji_dict[char][2:end] : char)
-        end
-        return out
+        String((c ∈ keys(emoji_dict) ? emoji_dict[c][2:end] : c for c in graphemes(x))...)
     end
     const latex_dict = Dict((v => k) for (k, v) in REPL.REPLCompletions.latex_symbols)
     function decode_latex(x)
-        out = ""
-        for char in graphemes(x)
-            out = out * (char ∈ keys(latex_dict) ? latex_dict[char][2:end] : char)
-        end
-        return out
+        String((c ∈ keys(latex_dict) ? latex_dict[c][2:end] : c for c in graphemes(x))...)
     end
     function unidecode(x)
         decode_emoji(decode_latex(x))
