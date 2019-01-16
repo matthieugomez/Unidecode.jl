eb0dfbbecb7d3ce9c290c96ad9c48c84b90e20c9
matthieugomez <gomez.matthieu@gmail.com>
2019-01-16 15:41:29 -0500

    work with string not just char

--- src/Unidecode.jl
+++ src/Unidecode.jl
@@ -1,12 +1,20 @@
 module Unidecode
-    using REPL
+    using REPL, Unicode
     const emoji_dict = Dict((v => k) for (k, v) in REPL.REPLCompletions.emoji_symbols)
     function decode_emoji(x)
-        x ∈ keys(emoji_dict) ? emoji_dict[x][2:end] : x
+        out = ""
+        for char in graphemes(x)
+            out = out * (char ∈ keys(emoji_dict) ? emoji_dict[char][2:end] : char)
+        end
+        return out
     end
     const latex_dict = Dict((v => k) for (k, v) in REPL.REPLCompletions.latex_symbols)
     function decode_latex(x)
-          x ∈ keys(latex_dict) ? latex_dict[x][2:end] : x
+        out = ""
+        for char in graphemes(x)
+            out = out * (char ∈ keys(latex_dict) ? latex_dict[char][2:end] : char)
+        end
+        return out
     end
     function unidecode(x)
         decode_emoji(decode_latex(x))
