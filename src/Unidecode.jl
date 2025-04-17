5a84ab5ac3b889934266c497b7842a666d7d9daa
Matthieu Gomez <gomez.matthieu@gmail.com>
2025-04-17 10:44:29 -0400

    update
    add char

--- src/Unidecode.jl
+++ src/Unidecode.jl
@@ -3,14 +3,15 @@ module Unidecode
     using Unicode: graphemes
     const emoji_dict = Dict((v => k[2:end]) for (k, v) in emoji_symbols)
     const latex_dict = Dict((v => k[2:end]) for (k, v) in latex_symbols)
-    function decode_emoji(x)
-        string((get(emoji_dict, c, c) for c in graphemes(x))...)
-    end
-    function decode_latex(x)
-        string((get(latex_dict, c, c) for c in graphemes(x))...)
-    end
-    function unidecode(x)
-        decode_emoji(decode_latex(x))
-    end
+    # path for characters
+    decode_emoji(c::Char) = get(emoji_dict, c, c)
+    decode_latex(c::Char) = get(latex_dict, c, c)
+    
+    # path for abstract strings
+    decode_emoji(x) = string((get(emoji_dict, c, c) for c in graphemes(x))...)
+    decode_latex(x) = string((get(latex_dict, c, c) for c in graphemes(x))...)
+    
+    unidecode(x) = decode_emoji(decode_latex(x))
+
     export decode_emoji, decode_latex, unidecode
 end
